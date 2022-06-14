041d711ab1bafd22b745d64acf09cb25657693da
mabus <m.buscaroli@outlook.com>
2022-06-14 23:05:44 +0100

    can now add cards and card link opens new tap/page

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from './index.module.css'
 
 function Card({ id, title, singer, genre, link, stars }) {
-  console.log('inside the card')
   return (
     <div key={id} className={styles.card}>
       <div className={styles.sphere}>{genre}</div>
@@ -12,7 +11,11 @@ function Card({ id, title, singer, genre, link, stars }) {
       </div>
       <div className={styles.bottomWrapper}>
         <div className={`${styles.title} ${styles.left}`}>{title}</div>
-        <div className={`${styles.link} ${styles.right}`}>{link}</div>
+        <div className={`${styles.link} ${styles.right}`}>
+          <a href={`http://${link}`} target="_blank">
+            Open Link
+          </a>
+        </div>
         <div className={`${styles.singer} ${styles.left}`}>{singer}</div>
         <div className={`${styles.stars} ${styles.right}`}>{stars}</div>
       </div>
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,27 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { v4 } from 'uuid'
+
 import styles from './index.module.css'
 
-function Form() {
+function Form({ onSongSubmit }) {
+  const [title, setTitle] = useState('')
+  const [singer, setSinger] = useState('')
+  const [link, setLink] = useState('')
+  const [genre, setGenre] = useState('')
+
+  const onTitleChange = (e) => {
+    e.preventDefault()
+
+    setTitle(e.target.value)
+  }
+  const onSingerChange = (e) => {
+    e.preventDefault()
+
+    setSinger(e.target.value)
+  }
+  const onGenreChange = (e) => {
+    e.preventDefault()
+
+    setGenre(e.target.value)
+  }
+  const onLinkChange = (e) => {
+    e.preventDefault()
+
+    setLink(e.target.value)
+  }
+
+  const onFormSubmit = (e) => {
+    e.preventDefault()
+
+    console.log('submitted')
+    const data = {
+      id: v4(),
+      title,
+      singer,
+      genre,
+      link,
+    }
+    setTitle('')
+    setSinger('')
+    setGenre('')
+    setLink('')
+
+    onSongSubmit(data)
+  }
+
   return (
-    <div className={styles.form}>
+    <form onSubmit={onFormSubmit} className={styles.form}>
       <div className={`${styles.inputWrapper} ${styles.left}`}>
         <label className={styles.label}>Title</label>
-        <input type="text" className={styles.input} />
+        <input
+          onChange={onTitleChange}
+          type="text"
+          className={styles.input}
+          value={title}
+        />
       </div>
 
       <div className={`${styles.inputWrapper} ${styles.right}`}>
         <label className={styles.label}>Genre</label>
-        <input className={styles.input} />
+        <input
+          onChange={onGenreChange}
+          className={styles.input}
+          value={genre}
+        />
       </div>
 
       <div className={`${styles.inputWrapper} ${styles.left}`}>
-        <label className={styles.label}>Author</label>
-        <input className={styles.input} />
+        <label className={styles.label}>Singer</label>
+        <input
+          onChange={onSingerChange}
+          className={styles.input}
+          value={singer}
+        />
       </div>
 
       <div className={`${styles.inputWrapper} ${styles.right}`}>
         <label className={styles.label}>Link</label>
-        <input className={styles.input} />
+        <input onChange={onLinkChange} className={styles.input} value={link} />
       </div>
 
       <input
@@ -29,7 +89,7 @@ function Form() {
         className={`${styles.btn} ${styles.fullWidth}`}
         value="Add"
       />
-    </div>
+    </form>
   )
 }
 
