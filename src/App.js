a089c140c80828c1df3a475182961e720773e3ac
mabus <m.buscaroli@outlook.com>
2022-06-16 11:59:35 +0100

    refactor App.js: move Form and List components into TrackMusicPage

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-// import './components/Form'
-import styles from './App.module.css'
 
-// import { List, Form, Title } from './components'
-import { Title } from './components'
-// import { storeSongs, loadSongs } from './api/storage'
 import { HomePage, TrackMusicPage, GetMusicDataPage } from './Pages'
+import styles from './App.module.css'
+import { Title } from './components'
 
 function App() {
-  // const [songs, setSongs] = useState(loadSongs())
-
-  // useEffect(() => {
-  //   loadSongs()
-  // }, [])
-
-  // useEffect(() => {
-  //   storeSongs(songs)
-  // }, [songs])
-
-  // const onDataSubmit = (data) => {
-  //   setSongs((prev) => [...prev, data])
-  // }
-
-  // const onDeleteSong = (id) => {
-  //   const newArr = songs.filter((song) => song.id !== id)
-  //   setSongs(newArr)
-  // }
-
   return (
     <div className={styles.appWrapper}>
       <Title />
@@ -41,6 +19,3 @@ function App() {
 }
 
 export default App
-
-// <Form onSongSubmit={onDataSubmit} />
-// <List songList={songs} onDelSong={onDeleteSong} />
--- src/Pages/TrackMusicPage/index.js
+++ src/Pages/TrackMusicPage/index.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Form, List } from '../../components'
+import { loadSongs, storeSongs } from '../../api/storage'
 
 function TrackMusicPage() {
-  return <div>TrackMusicPage</div>
+  const [songs, setSongs] = useState(loadSongs())
+
+  useEffect(() => {
+    loadSongs()
+  }, [])
+
+  useEffect(() => {
+    storeSongs(songs)
+  }, [songs])
+
+  const onDataSubmit = (data) => {
+    setSongs((prev) => [...prev, data])
+  }
+
+  const onDeleteSong = (id) => {
+    const newArr = songs.filter((song) => song.id !== id)
+    setSongs(newArr)
+  }
+
+  return (
+    <>
+      <Form onSongSubmit={onDataSubmit} />
+      <List songList={songs} onDelSong={onDeleteSong} />
+    </>
+  )
 }
 
 export default TrackMusicPage
